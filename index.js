82829976e83c3ac6663c675d0ba6c54b657145ac
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-10 21:03:02 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+// Initialize Express App
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("Car Rental server is running"))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
