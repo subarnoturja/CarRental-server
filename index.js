c701619e8fd375d9e56cac9ae18c6b64efdb6ca0
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-11 23:35:55 +0600

    booking router added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 // Initialize Express App
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.get('/', (req, res) => res.send("Car Rental server is running"))
 app.use('/api/user', userRouter)
 app.use('/api/owner', ownerRouter)
+app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
