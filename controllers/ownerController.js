ce3c9de8ab67912a64eefaed036f60273bab4055
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-11 23:49:34 +0600

    owner controller and routes updated

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -1,4 +1,5 @@
 import imageKit from "../configs/imagekit.js";
+import Booking from "../models/Booking.js";
 import Car from "../models/Car.js";
 import User from "../models/User.js";
 import fs from "fs";
@@ -118,6 +119,24 @@ export const getDashboardData = async (req, res) => {
         }
 
         const cars = await Car.find({ owner: _id })
+        const bookings = await Booking.find({ owner: _id }).populate('car').sort({ createdAt: -1 });
+
+        const pendingBookings = await Booking.find({ owner: _id, status: "pending" })
+        const completedBookings = await Booking.find({ owner: _id, status: "confirmed" })
+
+        // Calculate monthly Revenue from bookings where status is confirmed
+        const monthlyRevenue = bookings.slice().filter(booking => booking.status === 'confirmed').reduce((acc, booking) => acc + booking.price, 0)
+
+        const dashboardData = {
+            totalCars: cars.length,
+            totalBookings: bookings.length,
+            pendingBookings: pendingBookings.length,
+            completedBookings: completedBookings.length,
+            recentBookings: bookings.slice(0, 3),
+            monthlyRevenue
+        }
+        res.json({ success: true, dashboardData })
+
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message })
