de8f6fa80f167c67d567bb6a9a4e14f24909da75
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-11 23:49:57 +0600

    changes are made

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -142,3 +142,38 @@ export const getDashboardData = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to update user image
+export const updateUserImage = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        const imageFile = req.file;
+
+        // upload image to imageKit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imageKit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/users'
+        })
+
+        // optimization through imagekit URL transformation
+        var optimizedImageUrl = imageKit.url({
+            path: response.filePath,
+            transformation: [
+                {width: '1280'}, // Width resizing
+                {quality: 'auto'}, // Auto compression
+                {format: 'webp'} // convert to modern format
+            ]
+        })
+
+        const image = optimizedImageUrl;
+
+        await User.findByIdAndUpdate(_id, { image });
+        res.json({ success: true, message: "Image Updated!" })
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
